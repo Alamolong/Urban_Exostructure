163a08caf938068b944508f9f06387cfa960919b
Alamolong <103000076+Alamolong@users.noreply.github.com>
2022-04-14 01:05:13 -0400

    Update config.js

--- config.js
+++ config.js
@@ -13,10 +13,10 @@ var config = {
             alignment: '',
             title: '1973 ROCKEFELLER\'S DRUG LAWS',
             image: 'images/drugwar.jpg',
-            description: 'Prisons were widely viewed as a barbaric and ineffective means of controlling deviant behavior. Then, on January 3, 1973, Nelson Rockefeller, the governor of New York, gave a State of the State address demanding that every illegal-drug dealer be punished with a mandatory prison sentence of life without parole.”',
+            description: 'Prisons were widely viewed as ineffective means of controlling deviant behavior. On January 3rd, 1973, Nelson Rockefeller, governor of New York, gave a state address demanding that every illegal drug dealer be punished with a mandatory prison life sentence without parole.',
             location: {
                 center: [22.25, -3],
-                zoom: 4.8,
+                zoom: 4.7,
                 pitch: 0,
                 bearing: 0.00
             },
@@ -42,10 +42,10 @@ var config = {
             alignment: 'center',
             title: '1984 CUOMO\'S PRISON CONSTRUCTION',
             image: 'images/cuomo_construction.jpg',
-            description: '“Over the next twelve years Mario Cuomo added more prison beds in New York than all the previous governors in the state\'s history combined. Their total cost, including interest, would eventually reach about $7 billion.”',
+            description: 'Over the next twelve years Mario Cuomo added more prison beds in New York than all the previous governors in the state\'s history combined. Their total cost, including interest, would eventually reach about $7 billion.',
             location: {
                 center: [22.25, -14],
-                zoom: 4.8,
+                zoom: 4.7,
                 pitch: 0,
                 bearing:0
             },
@@ -72,10 +72,10 @@ var config = {
             alignment: 'center',
             title: '1994 NYC\'S BROKEN WINDOW POLICY',
             image: 'images/broken_window_policy.jpg',
-            description: 'The New York City Police Department implemented a “zero tolerance” policy for policing petty crimes in 1990. The 1990s was a time of significant decrease in crime in New York City, which was largely attributed to the policing approach\'s focus on maintaining community order and safety.',
+            description: 'The New York City Police Department implemented a “zero tolerance” policy for surveilling petty crimes in 1990. This decade saw a significant decrease in crime rates, which was largely attributed to the focus of policing on maintaining community order and safety.',
             location: {
                 center: [22.25, -24],
-                zoom: 4.8,
+                zoom: 4.7,
                 pitch: 0,
                 bearing: 0
             },
@@ -103,10 +103,10 @@ var config = {
             alignment: 'center',
             title: '2004 DRUG LAW REFORMS',
             image: 'images/drug_law_reform.jpg',
-            description: 'DiFiore ties the prison downturn both to drug law reforms — in 2004, 2005 and 2009 — and a recognition in law enforcement that alternatives like drug treatment were needed.',
+            description: 'Judge Janet DiFiore ties the prison downturn both to drug law reforms - in 2004, 2005, and 2009 - and a recognition within law enforcement that alternatives like drug treatment were needed.',
             location: {
                 center: [22.25, -34],
-                zoom: 4.8,
+                zoom: 4.7,
                 pitch: 0,
                 bearing: 0
             },
@@ -134,10 +134,10 @@ var config = {
             alignment: 'center',
             title: '2011 CUOMO\'S PRISON CLOSURE',
             image: 'images/cuomo_closure.jpg',
-            description: '"We are locking up fewer people. But then you need fewer facilities. And the shrinkage of that system is going to be something that has to be thought through and managed," Cuomo said.',
+            description: '“We are locking up fewer people. But then you need fewer facilities. And the shrinkage of that system is going to be something that has to be thought through and managed,” Cuomo said.',
             location: {
                 center: [22.25, -41],
-                zoom: 4.8,
+                zoom: 4.7,
                 pitch: 0,
                 bearing: 0
             },
@@ -165,10 +165,10 @@ var config = {
             alignment: 'center',
             title: '2021 HOCHUL\'s PRISON CLOSURE',
             image: 'images/Hochul.jpg',
-            description: '“I want to get creative with this, I don’t know if something can be used as a substance abuse treatment center. We don\'t need as many prisons.”',
+            description: '"I want to get creative with this," she said. "I don\'t know if something can be used as a substance abuse treatment center. We don\'t need as many prisons. The number of people incarcerated has gone down dramatically in our state."',
             location: {
                 center: [22.25, -51],
-                zoom: 4.8,
+                zoom: 4.7,
                 pitch: 0,
                 bearing: 0
             },
